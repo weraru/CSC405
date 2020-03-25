6d74e48c26f4e4fbcb0dfb4751df03bfe3490709
Aeroknight110 <46943273+Aeroknight110@users.noreply.github.com>
2020-03-24 23:00:52 -0500

    Set up for login from database

--- 405Proj/App/App/App/ViewModels/LoginViewModel.cs
+++ 405Proj/App/App/App/ViewModels/LoginViewModel.cs
@@ -2,13 +2,19 @@
 using System.ComponentModel;
 using System.Windows.Input;
 using Xamarin.Forms;
+using System.Net.Http;
+using System.Collections.Generic;
+
+
 namespace App.ViewModels
 {
     public class LoginViewModel : INotifyPropertyChanged
     {
+        private static readonly HttpClient client = new HttpClient();
         public Action DisplayInvalidLoginPrompt;
         public event PropertyChangedEventHandler PropertyChanged = delegate { };
         private string email = "watchmen@gmail.com";
+        public string id = "hi";
         public string Email
         {
             get { return email; }
@@ -34,7 +40,10 @@ namespace App.ViewModels
             SubmitCommand = new Command(OnSubmit);
         }
         public async void OnSubmit()
-        {
+        {       
+           // var values=new JavaScriptSerializer().Serialize({username, password});
+           // var content = new FormUrlEncodedContent(values);
+           // var response = await client.PostAsync("162.236.218.100:500/login", content);
             if (email != "watchmen@gmail.com" || password != "secret")
             {
                 DisplayInvalidLoginPrompt();
